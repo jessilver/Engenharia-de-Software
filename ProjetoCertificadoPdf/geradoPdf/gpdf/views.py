c2728d442d141b82fa415317a66901e66ede7350
Mellloj <jdesousamadeira@gmail.com>
2024-08-18 17:55:41 -0300

    Implementacao de formatacao de cpf e rg no formulario

--- ProjetoCertificadoPdf/geradoPdf/gpdf/views.py
+++ ProjetoCertificadoPdf/geradoPdf/gpdf/views.py
@@ -5,5 +5,13 @@ from django.views.generic import View
 class Index(View):
     def get(self, request):
         return render(request, 'index.html')
+    
+    def post(self, request):
+
+        nomeAluno = request.POST.get('nomeAluno')
+
+        print(nomeAluno)
+
+        return render(request, "index.html")
 
 
